ddc8ea23d31d0f645a7670e22f800b0b6c5151a5
millet <noeyza47@gmail.com>
2017-08-28 21:25:18 +0700

    Add files via upload

---
+++ LICENSE.md/student.java
@@ -0,0 +1,22 @@
+
+public class student {
+
+	
+		int sum;
+		
+		String name;
+		
+		int midterm;
+		int final1;
+		public void printlnfo() {
+			sum = midterm+final1;
+			
+			System.out.print(name + " ");	
+			System.out.println(sum);	
+			
+		}
+		
+		
+	}
+	
+
